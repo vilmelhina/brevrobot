c9329291f3f9b9c5bcfd66840599b2baaa0d8d86
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2021-09-29 14:49:40 +0200

    Add files via upload

--- max.c
+++ max.c
@@ -10,6 +10,16 @@
 int main( void )
 {
     int initialize = initialize();
+    
+    /*Registrerar en sensor på sensor variabeln*/
+    touchSensor = sensor_search( LEGO_EV3_TOUCH ); //
+    gyroSensor = sensor_search(LEGO_EV3_GYRO);
+    /*Man kan antingen använda "Brick" bibliotekets förbyggda modes*/
+    touch_set_mode_touch(touchSensor);
+    
+    /*eller kolla i sensor.h filen på vilka modes som finns för alla olika sensorer*/
+    sensor_set_mode(gyroSensor, LEGO_EV3_GYRO_GYRO_G_AND_A);
+    
     while(!TouchReturnValue){
         TouchReturnValue = sensor_get_value(0, touchSensor, 0);
         
--- max.h
+++ max.h
@@ -32,14 +32,6 @@ int initialize(){
         brick_uninit();
         return(0);
     }
-    /*Registrerar en sensor på sensor variabeln*/
-    touchSensor = sensor_search( LEGO_EV3_TOUCH ); //
-    gyroSensor = sensor_search(LEGO_EV3_GYRO);
-    /*Man kan antingen använda "Brick" bibliotekets förbyggda modes*/
-    touch_set_mode_touch(touchSensor);
-    
-    /*eller kolla i sensor.h filen på vilka modes som finns för alla olika sensorer*/
-    sensor_set_mode(gyroSensor, LEGO_EV3_GYRO_GYRO_G_AND_A);
 }
 
 int getAngle(){
