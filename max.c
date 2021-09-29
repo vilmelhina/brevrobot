8ff4e5dd9dfae75d26bc1c30a24ccc0a5dbdab77
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2021-09-29 14:55:58 +0200

    Add files via upload

--- max.c
+++ max.c
@@ -5,11 +5,41 @@
 //  Created by Vilhelmina Andersson on 2021-09-29.
 //
 
-#include "max.h"
+#ifndef max_h
+#define max_h
+
+#include <stdio.h>
+#include "brick.h"
+#include <unistd.h>
+
+#define Sleep( msec ) usleep(( msec ) * 1000 ) /*Definerar sleep där Sleep(1000)= 1 sekund*/
+
+#define MOTOR_A            OUTA
+#define MOTOR_B            OUTB
+#define MOTOR_C            OUTC
+#define MOTOR_D            OUTD
+#define SENSOR_TOUCH    IN1
+#define SENSOR_GYRO        IN2
+#define SENSOR_3        IN3
+#define SENSOR_4        IN4
+
+POOL_T touchSensor;
+int TouchReturnValue = 0;
+POOL_T gyroSensor;
+
+int getAngle();
 
 int main( void )
 {
-    int initialize = initialize();
+    if ( !brick_init()) return ( 1 ); /*Instansierar klossen och tillåter remote-connection*/
+    printf( "*** ( EV3 ) Hello! ***\n" );
+    Sleep(2000);
+
+    if(!sensor_is_plugged((SENSOR_TOUCH|SENSOR_GYRO), SENSOR_TYPE__NONE_)) {
+        printf("Stoppa in sensorer i port 1 och 2\n");
+        brick_uninit();
+        return(0);
+    }
     
     /*Registrerar en sensor på sensor variabeln*/
     touchSensor = sensor_search( LEGO_EV3_TOUCH ); //
@@ -23,7 +53,7 @@ int main( void )
     while(!TouchReturnValue){
         TouchReturnValue = sensor_get_value(0, touchSensor, 0);
         
-        printf("Gyro0: %d , Gyro1: %d \n", getAngle() );
+        printf("Gyro: %d\n", getAngle() );
     
     }//Så länge touch sensorn inte är intryckt kommer while loopen köras
     
@@ -32,4 +62,9 @@ int main( void )
     printf( "dying...\n" );
     return ( 0 );
 
+}
+
+int getAngle(){
+    int angle = sensor_get_value(0, gyroSensor, 0);
+    return angle;
 }
