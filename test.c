f6e211db03604ddaa8e657569ce18390fc26c794
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2021-09-30 10:38:46 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,59 +1,54 @@
-//
-//  max.c
-//  testar alla de simpla funktionerna som vi ska skriva
-//
-//  Created by Vilhelmina Andersson on 2021-09-29.
-//
-
 #include <stdio.h>
 #include "brick.h"
 #include <unistd.h>
 
-#define Sleep( msec ) usleep(( msec ) * 1000 ) /*Definerar sleep där Sleep(1000)= 1 sekund*/
+#define Sleep( msec ) usleep(( msec ) * 1000 ) /* Definerar sleep, Sleep(1000)= 1 sekund */
 
-#define MOTOR_RIGHT        OUTA
-#define MOTOR_LEFT        OUTB
-#define MOTOR_C            OUTC
-#define MOTOR_D            OUTD
-#define SENSOR_TOUCH    IN1
-#define SENSOR_GYRO        IN2
-#define SENSOR_3        IN3
-#define SENSOR_4        IN4
-#define MOTOR_BOTH         ( MOTOR_LEFT | MOTOR_RIGHT ) /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
+#define MOTOR_RIGHT    	OUTA
+#define MOTOR_LEFT    	OUTB
+#define MOTOR_C    		OUTC
+#define MOTOR_D    		OUTD
+#define SENSOR_TOUCH	IN1
+#define SENSOR_2		IN2
+#define SENSOR_3		IN3
+#define SENSOR_4		IN4
 
-POOL_T touchSensor;
-int TouchReturnValue = 0;
+#define MOTOR_BOTH     	( MOTOR_LEFT | MOTOR_RIGHT ) /* Bitvis ELLER ger att båda motorerna styrs samtidigt */
+
 int max_hastighet;         /* variabel för max hastighet på motorn */
-POOL_T gyroSensor;
-
+POOL_T touchSensor;
 
 int main( void )
-{
-    if ( !brick_init()) return ( 1 ); /*Instansierar klossen och tillåter remote-connection*/
-    printf( "*** ( EV3 ) Hello! ***\n" );
-    Sleep(2000);
-
-    if(!sensor_is_plugged((SENSOR_TOUCH|SENSOR_GYRO), SENSOR_TYPE__NONE_)) {
-        printf("Stoppa in sensorer i port 1 och 2\n");
-        brick_uninit();
-        return(0);
+{  
+	
+	if ( !brick_init()) return ( 1 ); /* Initialiserar EV3-klossen */
+	printf( "*** ( EV3 ) Hello! ***\n" );
+	Sleep( 2000 );
+	
+	if ( tacho_is_plugged( MOTOR_BOTH, TACHO_TYPE__NONE_ )) {  /* TACHO_TYPE__NONE_ = Alla typer av motorer */
+        max_hastighet = tacho_get_max_speed( MOTOR_LEFT, 0 );	/* Kollar maxhastigheten som motorn kan ha */
+        tacho_reset( MOTOR_BOTH );
+    } else {
+        printf( "Anslut vänster motor i port A,\n"
+        "Anslut höger motor i port B.\n"
+        );
+      	brick_uninit();
+        return ( 0 );  /* Stänger av sig om motorer ej är inkopplade */
     }
-    
-    touchSensor = sensor_search( LEGO_EV3_TOUCH );  // Registrerar en touch sensor på touchSensor-variabeln
-    gyroSensor = sensor_search(LEGO_EV3_GYRO);
-    touch_set_mode_touch(touchSensor); //anger vilken "mode" sensorn skall ha
-    sensor_set_mode(gyroSensor, LEGO_EV3_GYRO_GYRO_G_AND_A); /*eller kolla i sensor.h filen på vilka modes som finns för alla olika sensorer*/
-    
-    tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.3 );  // Sätter hastigheten på båda motorerna till 50% av maxhastigheten
-    
-    tacho_run_forever(  MOTOR_BOTH );
+	
+	touchSensor = sensor_search( LEGO_EV3_TOUCH ); // Registrerar en touch sensor på touchSensor-variabeln
+	touch_set_mode_touch(touchSensor); // anger vilken "mode" sensorn skall ha
+
+	tacho_set_speed_sp( MOTOR_BOTH, max_hastighet * 0.3 );  // Sätter hastigheten på båda motorerna till 50% av maxhastigheten
+	/* Om man vill köra bakåt anger man negativ hastighet, till exempel max_hastighet * (-0.5) */
+	
+	tacho_run_forever(  MOTOR_BOTH );
+	Sleep( 5000 ); 
+	tacho_stop( MOTOR_BOTH );
+
+	brick_uninit();
+	printf( "dying...\n" );
     Sleep( 5000 );
-    tacho_stop( MOTOR_BOTH );
-    
-    brick_uninit();
-    printf( "dying...\n" );
     return ( 0 );
 
 }
-
-
