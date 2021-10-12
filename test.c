2a61b937f65b844a0bcbd0df8cb10ed42693e505
vilmelhina <90849312+vilmelhina@users.noreply.github.com>
2021-10-12 14:23:14 +0200

    Update and rename distance_test.c to test.c

--- test.c
+++ test.c
@@ -11,12 +11,14 @@ int main( void )
 {
     int nearest_wall;
     int distance;
+    int angle;
     
     int initialize = initialize_max();
     
     while(1){
         distance = get_distance();
-        printf("Distance: %d \n", distance);
+        angle = get_angle();
+        printf("Distance: %d Angle: %d\n", distance, angle);
     }
     
     brick_uninit(); //funktion från biblioteket
